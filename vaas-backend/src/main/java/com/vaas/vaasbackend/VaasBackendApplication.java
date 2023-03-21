7187d3f83d7ae5901474fd420b94b537067718f2
BBDNET\bbdnet10225 <miren@bbd.co.za>
2023-03-21 10:53:26 +0530

    add backend component of vaas system

---
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/VaasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaas.vaasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaasBackendApplication.class, args);
+	}
+
+}
---
+++ vaas-backend/src/test/java/com/vaas/vaasbackend/VaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaas.vaasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
