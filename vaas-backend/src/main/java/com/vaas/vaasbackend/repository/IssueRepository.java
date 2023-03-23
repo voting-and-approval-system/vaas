7ae1d8a965edb138215ef25b2d321cb8171b4ec6
BBDNET\bbdnet10225 <miren@bbd.co.za>
2023-03-23 14:59:16 +0530

    add more apis for issue

--- vaas-backend/src/main/java/com/vaas/vaasbackend/repository/IssueRepository.java
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/repository/IssueRepository.java
@@ -4,6 +4,10 @@ import com.vaas.vaasbackend.entity.TblIssue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends JpaRepository<TblIssue,Integer> {
+
+    public List<TblIssue> findByIssueIsActive(boolean isActive);
 }
--- vaas-backend/src/main/java/com/vaas/vaasbackend/service/IssueServiceImpl.java
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/service/IssueServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class IssueServiceImpl implements IssueService{
@@ -17,6 +18,11 @@ public class IssueServiceImpl implements IssueService{
     }
 
     @Override
+    public TblIssue ShowIssue(Integer id) {
+        return issueRepository.findById(id).get();
+    }
+
+    @Override
     public TblIssue SaveIssue(TblIssue issue) {
         return issueRepository.save(issue);
     }
@@ -24,5 +30,52 @@ public class IssueServiceImpl implements IssueService{
     @Override
     public void DeleteIssue(Integer id) {
         issueRepository.deleteById(id);
+    }
+
+    @Override
+    public TblIssue UpdateIssue(Integer id, TblIssue issue) {
+        TblIssue tblIssue = issueRepository.findById(id).get();
+
+        if(Objects.nonNull(issue.getIssueTitle()) &&
+                !"".equalsIgnoreCase(issue.getIssueTitle())) {
+            tblIssue.setIssueTitle(issue.getIssueTitle());
+        }
+
+        if(Objects.nonNull(issue.getIssueDescription()) &&
+                !"".equalsIgnoreCase(issue.getIssueDescription())) {
+            tblIssue.setIssueDescription(issue.getIssueDescription());
+        }
+
+        if(Objects.nonNull(issue.getIssueAttachmentPath()) &&
+                !"".equalsIgnoreCase(issue.getIssueAttachmentPath())) {
+            tblIssue.setIssueAttachmentPath(issue.getIssueAttachmentPath());
+        }
+
+        if(Objects.nonNull(issue.getAllowMultipleOptions()) &&
+                !"".equalsIgnoreCase(issue.getAllowMultipleOptions().toString())) {
+            tblIssue.setAllowMultipleOptions(issue.getAllowMultipleOptions());
+        }
+
+        if(Objects.nonNull(issue.getAssetes()) &&
+                !"".equalsIgnoreCase(issue.getAssetes().toString())) {
+            tblIssue.setAssetes(issue.getAssetes());
+        }
+
+        if(Objects.nonNull(issue.getIssueIsActive()) &&
+                !"".equalsIgnoreCase(issue.getIssueIsActive().toString())) {
+            tblIssue.setIssueIsActive(issue.getIssueIsActive());
+        }
+
+        if(Objects.nonNull(issue.getVoteType()) &&
+                !"".equalsIgnoreCase(issue.getVoteType().toString())) {
+            tblIssue.setVoteType(issue.getVoteType());
+        }
+
+        return issueRepository.save(tblIssue);
+    }
+
+    @Override
+    public List<TblIssue> ShowActiveIssue(boolean isActive) {
+        return issueRepository.findByIssueIsActive(isActive);
     }
 }
