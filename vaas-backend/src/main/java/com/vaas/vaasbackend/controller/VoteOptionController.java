d0d96bd55b4d887c11082f436b9bc1aa9582f83e
Patel Nilay <101557733+Randomnilay@users.noreply.github.com>
2023-04-05 14:43:42 +0530

    rsult vcontroller added

--- vaas-backend/src/main/java/com/vaas/vaasbackend/controller/VoteOptionController.java
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/controller/VoteOptionController.java
@@ -1,11 +1,10 @@
 package com.vaas.vaasbackend.controller;
 
+import com.vaas.vaasbackend.entity.TblVoteOption;
 import com.vaas.vaasbackend.responseBody.TotalVoteForIssue;
 import com.vaas.vaasbackend.service.VoteOptionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,7 +13,17 @@ public class VoteOptionController {
     @Autowired
     VoteOptionService voteOptionService;
 
-    @GetMapping("voteoption/findtotalvote/{issueId}/{roundNumber}")
+    @GetMapping("/voteoption")
+    public List<TblVoteOption> showVoteOption(){
+        return voteOptionService.showVoteOption();
+    }
+
+    @PostMapping("/voteoption")
+    public TblVoteOption saveVoteOption(@RequestBody TblVoteOption tblVoteOption){
+        return voteOptionService.saveVoteOption(tblVoteOption);
+    }
+
+    @GetMapping("/voteoption/findtotalvote/{issueId}/{roundNumber}")
     public List<TotalVoteForIssue> countTotalVotes(@PathVariable(name = "issueId") Integer issueId,@PathVariable(name = "roundNumber") Integer roundNumber){
         return voteOptionService.countTotalVotes(issueId,roundNumber);
     }
--- vaas-backend/src/main/java/com/vaas/vaasbackend/service/VoteOptionService.java
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/service/VoteOptionService.java
@@ -1,5 +1,6 @@
 package com.vaas.vaasbackend.service;
 
+import com.vaas.vaasbackend.entity.TblVoteOption;
 import com.vaas.vaasbackend.repository.VoteOptionRepository;
 import com.vaas.vaasbackend.responseBody.TotalVoteForIssue;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,21 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class VoteOptionService {
+public class VoteOptionService{
     @Autowired
     VoteOptionRepository voteOptionRepository;
 
     public List<TotalVoteForIssue> countTotalVotes(Integer issueId, Integer roundNumber) {
         return voteOptionRepository.countTotalVotes(issueId,roundNumber);
+    }
+
+
+    public List<TblVoteOption> showVoteOption() {
+        return voteOptionRepository.findAll();
+    }
+
+
+    public TblVoteOption saveVoteOption(TblVoteOption tblVoteOption) {
+        return voteOptionRepository.save(tblVoteOption);
     }
 }
