24661ce4fdfaab7b77197ced99b401771936518f
miren1303 <miren@bbd.co.za>
2023-04-05 14:39:36 +0530

    solve errors

--- vaas-backend/src/main/java/com/vaas/vaasbackend/service/VoteOptionService.java
+++ vaas-backend/src/main/java/com/vaas/vaasbackend/service/VoteOptionService.java
@@ -1,5 +1,6 @@
 package com.vaas.vaasbackend.service;
 
+import com.vaas.vaasbackend.entity.TblVoteOption;
 import com.vaas.vaasbackend.repository.VoteOptionRepository;
 import com.vaas.vaasbackend.responseBody.TotalVoteForIssue;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,5 +15,13 @@ public class VoteOptionService {
 
     public List<TotalVoteForIssue> countTotalVotes(Integer issueId, Integer roundNumber) {
         return voteOptionRepository.countTotalVotes(issueId,roundNumber);
+    }
+
+    public List<TblVoteOption> showVoteOption() {
+        return voteOptionRepository.findAll();
+    }
+
+    public TblVoteOption saveVoteOption(TblVoteOption tblVoteOption) {
+        return voteOptionRepository.save(tblVoteOption);
     }
 }
