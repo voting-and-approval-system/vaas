7ba50ff009cb0a5df4fdd0e8a3cdc6703938c6f7
Patel Nilay <101557733+Randomnilay@users.noreply.github.com>
2023-05-07 00:24:45 +0530

    login added without google

--- vaas-frontend/src/app/_services/user-auth.service.ts
+++ vaas-frontend/src/app/_services/user-auth.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserAuthService {
+  constructor() {}
 
-  constructor() { }
-
-  public setRoles(roles: []){
+  public setRoles(roles: any[]) {
     localStorage.setItem('roles', JSON.stringify(roles));
   }
 
-  public getRoles(): [] {
-   return JSON.parse(localStorage.getItem('roles'));
+  public getRoles(): any[] {
+    const rolesString = localStorage.getItem('roles');
+    return rolesString ? JSON.parse(rolesString) : [];
+  }
+
+  public setToken(jwtToken: string) {
+    localStorage.setItem('jwtToken', jwtToken);
+  }
+
+  public getToken(): string | null {
+    return localStorage.getItem('jwtToken');
+  }
+
+  public clear() {
+    localStorage.clear();
+  }
+
+  public isLoggedIn() {
+    return this.getRoles().length > 0 && this.getToken() !== null;
   }
 }
--- vaas-frontend/src/app/login/login.component.ts
+++ vaas-frontend/src/app/login/login.component.ts
@@ -1,29 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserAuthService } from '../_services/user-auth.service';
 import { UsersService } from '../_services/users.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+  constructor(
+    private userService: UsersService,
+    private userAuthService: UserAuthService,
+    private router: Router
+  ) {}
 
-  constructor(private usersService: UsersService){
-    
-  }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+  login(loginForm: NgForm) {
+    this.userService.login(loginForm.value).subscribe(
+      (response: any) => {
+        this.userAuthService.setRoles(response.roles);
+        this.userAuthService.setToken(response.jwtToken);
 
-  login(loginForm:NgForm)
-  {
-    this.usersService.login(loginForm.value).subscribe(
-      (response)=>{
-        console.log(response);
+        const role = response.roles[0];
+        if (role === 'Admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/user']);
+        }
       },
-      (error)=>{
+      (error) => {
         console.log(error);
       }
     );
