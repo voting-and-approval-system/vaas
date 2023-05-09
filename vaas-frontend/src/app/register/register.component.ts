bc76a2d85104a98023a0ba64778821bc628984aa
miren1303 <miren@bbd.co.za>
2023-05-09 17:58:48 +0530

    add new component

---
+++ vaas-frontend/src/app/register/register.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  userForm : FormGroup;
+
+  @Output() formSubmitted = new EventEmitter<{ houseNumber: number, phoneNumber: string }>();
+
+  constructor(private fb : FormBuilder){
+    this.userForm = this.fb.group({
+      HouseNo : '',
+      PhNo : ''})
+  }
+
+  registerUser(){
+    const houseNumber = this.userForm.get('HouseNo').value;
+    const phoneNumber = this.userForm.get('PhNo').value;
+
+    this.formSubmitted.emit({ houseNumber, phoneNumber })
+  }
+}
