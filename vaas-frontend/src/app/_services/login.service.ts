be7103f790fb783ca9a043ffae659144a9295c6a
miren1303 <miren@bbd.co.za>
2023-05-29 12:26:36 +0530

    Fix bug : when open website for first time sign in button not work is now fixed

--- vaas-frontend/src/app/_services/login.service.ts
+++ vaas-frontend/src/app/_services/login.service.ts
@@ -13,77 +13,69 @@ declare const gapi: any;
 })
 export class LoginService {
   userEmail!: string;
-  public tempRole : string;
+  public tempRole: string;
 
 
   constructor(
     private userService: UsersService,
     private userAuthService: UserAuthService,
     private router: Router,
-    private register : MatDialog
-  ) {}
+    private register: MatDialog
+  ) { }
 
-  openForm(newUser : any,userEmail : string) {
+  openForm(newUser: any, userEmail: string) {
     const dialogRef = this.register.open(RegisterComponent);
-    dialogRef.componentInstance.formSubmitted.subscribe(({ houseNumber, phoneNumber }) => {         
-          if(houseNumber == null || phoneNumber == null){
-            return;
-          }
-          newUser.houseNumber = houseNumber || '';
-          newUser.phoneNumber = phoneNumber || '';
-          this.userService.register(newUser).subscribe(
-            (response: any) => {
-              this.loginWithGoogle(userEmail);
-            },
-            (error: any) => {
-            }
-          );
+    dialogRef.componentInstance.formSubmitted.subscribe(({ houseNumber, phoneNumber }) => {
+      if (houseNumber == null || phoneNumber == null) {
+        return;
+      }
+      newUser.houseNumber = houseNumber || '';
+      newUser.phoneNumber = phoneNumber || '';
+      this.userService.register(newUser).subscribe(
+        (response: any) => {
+          this.loginWithGoogle(userEmail);
+        },
+        (error: any) => {
+        }
+      );
     });
   }
-  
 
-  loadGoogleApi(): void {
+  signInWithGoogle(): void {
     gapi.load('auth2', () => {
       gapi.auth2.init({
-        client_id: '374191778060-j6pbqqlneq1hv5c8lijgmj6ihhkf12gi.apps.googleusercontent.com', 
-        scope: 'email', 
+        client_id: '374191778060-j6pbqqlneq1hv5c8lijgmj6ihhkf12gi.apps.googleusercontent.com',
+        scope: 'email',
         plugin_name: 'vaas-frontend',
         prompt: 'select_account'
-      }).then((error:any)=>{
-        console.log("Success");
-      }).catch((error:any)=>{
+      }).then(() => {
+        Promise.resolve(gapi.auth2.getAuthInstance().signIn())
+          .then((googleUser: any) => {
+            const userEmail = googleUser.getBasicProfile().getEmail();
+            this.loginWithGoogle(userEmail);
+          })
+          .catch((error: any) => {
+            if (error.details === 'popup_closed_by_user') {
+              console.log('User closed the Google sign-in popup');
+            } else {
+              console.log('Error occurred during Google sign-in', error);
+            }
+          });
+      }).catch((error: any) => {
         console.log("Error");
       });
     });
-   
   }
-  
-
-  signInWithGoogle(): void {
-    Promise.resolve(gapi.auth2.getAuthInstance().signIn())
-      .then((googleUser: any) => {
-        const userEmail = googleUser.getBasicProfile().getEmail();
-        this.loginWithGoogle(userEmail);
-      })
-      .catch((error: any) => {
-        if (error.details === 'popup_closed_by_user') {
-          console.log('User closed the Google sign-in popup');
-        } else {
-          console.log('Error occurred during Google sign-in', error);
-        }
-      });
-  }
-  
 
   loginWithGoogle(userEmail: string): void {
-    localStorage.setItem('userEmail',userEmail);
+    localStorage.setItem('userEmail', userEmail);
 
     const googleUser = gapi.auth2.getAuthInstance().currentUser.get();
     const profile = googleUser.getBasicProfile();
-  
+
     const firstName = profile.getGivenName();
     const lastName = profile.getFamilyName();
-  
+
     const newUser = {
       firstName: firstName,
       lastName: lastName,
@@ -103,33 +95,32 @@ export class LoginService {
         const role = response.roles;
         if (role.includes('Admin') && role.includes('Tenant')) {
           this.router.navigate(['/adminoption']);
-        } 
-        else if(role.includes('Admin'))
-        {
+        }
+        else if (role.includes('Admin')) {
           this.tempRole = "Admin";
           this.userAuthService.setPreferdRole('Admin');
           this.router.navigate(['/admin']);
         }
-        else if(role.includes('Tenant')){
+        else if (role.includes('Tenant')) {
           this.userAuthService.setPreferdRole('Tenant');
           this.tempRole = "Tenant";
           this.router.navigate(['/user']);
         }
-        else{
+        else {
           this.router.navigate(['/forbidden']);
         }
       },
       (error) => {
         if (error.status === 500) {
-          this.openForm(newUser,userEmail);
+          this.openForm(newUser, userEmail);
         } else {
           console.log('Error occurred during login', error);
         }
       }
-      
+
     );
 
-    
+
   }
   private logoutAndNotifyGoogle(): void {
     localStorage.removeItem('userEmail');
@@ -143,11 +134,11 @@ export class LoginService {
   checkTokenExpiration(): void {
     const jwtToken = localStorage.getItem('jwtToken');
     const tokenExpiration = localStorage.getItem('tokenExpiration');
-  
+
     if (jwtToken && tokenExpiration) {
       const expirationTime = new Date(tokenExpiration).getTime();
       const currentTime = new Date().getTime();
-  
+
       if (currentTime >= expirationTime) {
         this.logoutAndNotifyGoogle();
         this.router.navigate(['/home']);
