d77ee444c02a4e739641f97ab4775648ec3039bd
Patel Nilay <101557733+Randomnilay@users.noreply.github.com>
2023-06-12 15:34:59 +0530

    configs

--- vaas-frontend/src/app/user/user-services.service.ts
+++ vaas-frontend/src/app/user/user-services.service.ts
@@ -12,7 +12,7 @@ export class UserServicesService {
     .set('Authorization', `Bearer ${this._userAuthService.getToken()}`)
     .set('Content-Type', 'application/json');
 
-  baseUrl = "http://localhost:8080";
+  baseUrl = "https://ec2-34-203-243-194.compute-1.amazonaws.com:8080";
 
   findUserByEmail(userEmail: string) {
     return this._http.get(this.baseUrl + `/users/${userEmail}`, { headers: this.headers });
